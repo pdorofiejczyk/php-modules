20bf1280f1d99aadc4c021c1521f1e73b0efd691
Paweł Dorofiejczyk <nikopool@gmail.com>
2014-02-01 22:02:25 +0100

    delete "Module" form methods names

--- Modules.php
+++ Modules.php
@@ -12,7 +12,7 @@ final class Modules {
 	private static $instances = array();
 	private static $includePath = '';
 
-	public static function importModule($name) {
+	public static function import($name) {
 		if(array_key_exists($name, self::$instances)) {
 			return self::$instances[$name];
 		} else if(array_key_exists($name, self::$modules)) {
@@ -27,7 +27,7 @@ final class Modules {
 		}
 	}
 
-	public static function registerModule($name, $path) {
+	public static function register($name, $path) {
 		self::$modules[$name] = $path;
 	}
 
--- test/test.php
+++ test/test.php
@@ -2,9 +2,9 @@
 include(dirname(__FILE__) . '/../Modules.php');
 
 Modules::setIncludePath(dirname(__FILE__));
-Modules::registerModule('Test', 'modules/testModule.php');
+Modules::register('Test', 'modules/testModule.php');
 
-$MyTestModule = Modules::importModule('Test');
+$MyTestModule = Modules::import('Test');
 
 print_r($MyTestModule->y());
 ?>
